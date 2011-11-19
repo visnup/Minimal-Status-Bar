338cf6ebd4e78484d29fec2b49c6428be9187337
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-11-18 17:19:07 -0800

    refactor a bit

--- inject.js
+++ inject.js
@@ -25,32 +25,35 @@ if (window.top === window) {
       document.body.appendChild(statusBar);
     }
 
-    var href = el.attributes.href.value
-      , email = href.match(/^mailto:([^?]+)(\?subject=([^&]+))?/i);
-    if (email) {
-      statusBar.innerText = 'Send email to ' + email[1];
-      if (email[3])
-        statusBar.innerText += ' with subject “' + decodeURIComponent(email[3]) + '”';
-    } else if (e.metaKey) {
-      if (e.altKey) {
-        statusBar.innerText = 'Open “' + href + '” in a new window behind the current window';
-      } else {
-        statusBar.innerText = 'Open “' + href + '” in a new tab behind the current one';
-      }
-    } else if (e.altKey) {
-      statusBar.innerText = 'Download “' + href + '”';
-    } else if (e.ctrlKey) {
-      statusBar.innerText = 'Display a menu for “' + href + '”';
-    } else if (el.target) {
-      statusBar.innerText = 'Open “' + href + '” in a new tab';
-    } else {
-      statusBar.innerText = 'Go to “' + href + '”';
-    }
+    statusBar.innerText = getStatus(el, e);
     setTimeout(function() { statusBar.className = 'active'; }, 1);
   }
 
   function hideStatus() {
     if (statusBar)
       statusBar.className = '';
+  }
+
+  function getStatus(el, e) {
+    var href = el.attributes.href.value
+      , email = href.match(/^mailto:([^?]+)(\?subject=([^&]+))?/i);
+
+    if (email) {
+      return 'Send email to ' + email[1] + (email[3] ? ' with subject “' + decodeURIComponent(email[3]) + '”' : '');
+    } else if (e.metaKey) {
+      if (e.altKey) {
+        return 'Open “' + href + '” in a new window behind the current window';
+      } else {
+        return 'Open “' + href + '” in a new tab behind the current one';
+      }
+    } else if (e.altKey) {
+      return 'Download “' + href + '”';
+    } else if (e.ctrlKey) {
+      return 'Display a menu for “' + href + '”';
+    } else if (el.target) {
+      return 'Open “' + href + '” in a new tab';
+    } else {
+      return 'Go to “' + href + '”';
+    }
   }
 }
