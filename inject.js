5f7edd67a71d5d5d39c6741517d4874b562a516a
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-11-20 23:32:30 -0800

    longurl support

--- inject.js
+++ inject.js
@@ -1,11 +1,19 @@
 var statusBar;
 
 if (window.top === window) {
-  function ready() { document.body.addEventListener('mouseover', hover); }
   if (document.readyState === 'complete')
     ready();
   else
     document.addEventListener('DOMContentLoaded', ready);
+
+  safari.self.addEventListener('message', function handleMessage(e) {
+    switch (e.name) {
+      case 'displayStatus':
+        displayStatus(e.message);
+    }
+  }, false);
+
+  function ready() { document.body.addEventListener('mouseover', hover); }
 
   function hover(e) {
     var el = e.target;
@@ -13,47 +21,30 @@ if (window.top === window) {
       el = el.parentNode;
 
     if (el && el.attributes.href)
-      displayStatus(el, e);
+      safari.self.tab.dispatchMessage('hover', {
+        href: el.attributes.href.value,
+        target: el.target,
+        metaKey: e.metaKey,
+        ctrlKey: e.ctrlKey,
+        altKey: e.altKey
+      });
     else
       hideStatus();
   }
 
-  function displayStatus(el, e) {
+  function displayStatus(text) {
     if (!statusBar) {
       statusBar = document.createElement('div');
       statusBar.id = 'com-fortnight-status-bar';
       document.body.appendChild(statusBar);
     }
 
-    statusBar.innerText = getStatus(el, e);
+    statusBar.innerText = text;
     setTimeout(function() { statusBar.className = 'active'; }, 1);
   }
 
   function hideStatus() {
     if (statusBar)
       statusBar.className = '';
-  }
-
-  function getStatus(el, e) {
-    var href = el.attributes.href.value
-      , email = href.match(/^mailto:([^?]+)(\?subject=([^&]+))?/i);
-
-    if (email) {
-      return 'Send email to ' + email[1] + (email[3] ? ' with subject “' + decodeURIComponent(email[3]) + '”' : '');
-    } else if (e.metaKey) {
-      if (e.altKey) {
-        return 'Open “' + href + '” in a new window behind the current window';
-      } else {
-        return 'Open “' + href + '” in a new tab behind the current one';
-      }
-    } else if (e.altKey) {
-      return 'Download “' + href + '”';
-    } else if (e.ctrlKey) {
-      return 'Display a menu for “' + href + '”';
-    } else if (el.target) {
-      return 'Open “' + href + '” in a new tab';
-    } else {
-      return 'Go to “' + href + '”';
-    }
   }
 }
